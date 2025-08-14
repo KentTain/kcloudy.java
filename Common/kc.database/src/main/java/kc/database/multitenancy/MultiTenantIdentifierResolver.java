119f490a739858da2628228477ac18a5c395dbab
田长军 <tianchangjun@outlook.com>
2025-08-14 15:45:07 +0800

    Account项目的启动类路径不对

--- Common/kc.database/src/main/java/kc/database/multitenancy/MultiTenantIdentifierResolver.java
+++ Common/kc.database/src/main/java/kc/database/multitenancy/MultiTenantIdentifierResolver.java
@@ -1,6 +1,5 @@
 package kc.database.multitenancy;
 
-
 import org.hibernate.context.spi.CurrentTenantIdentifierResolver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,27 +10,27 @@ import kc.framework.tenant.TenantContext;
 
 /**
  * 这个类是由Hibernate提供的用于识别tenantId的类，当每次执行sql语句被拦截就会调用这个类中的方法来获取tenantId
+ * 
  * @author lanyuanxiaoyao
  * @version 1.0
- * https://dzone.com/articles/spring-boot-hibernate-multitenancy-implementation
+ *          https://dzone.com/articles/spring-boot-hibernate-multitenancy-implementation
  */
 @Component
-public class MultiTenantIdentifierResolver implements CurrentTenantIdentifierResolver{
-	private Logger logger = LoggerFactory.getLogger(MultiTenantIdentifierResolver.class.getName());
-    
-	// 获取tenantId的逻辑在这个方法里面写
-	@Override
+public class MultiTenantIdentifierResolver implements CurrentTenantIdentifierResolver {
+    private Logger logger = LoggerFactory.getLogger(MultiTenantIdentifierResolver.class.getName());
+
+    // 获取tenantId的逻辑在这个方法里面写
+    @Override
     public String resolveCurrentTenantIdentifier() {
         Tenant tenant = TenantContext.getCurrentTenant();
         if (tenant != null) {
-        	logger.debug("-----MultiTenantIdentifierResolver get tenant: " + tenant.getTenantName());
+            logger.debug("-----MultiTenantIdentifierResolver get tenant: " + tenant.getTenantName());
             return tenant.getTenantName();
         }
-        
-        logger.debug("-----MultiTenantIdentifierResolver default tenant: " + TenantDataSourceProvider.DEFAULT_TENANT_NAME);
+
         return TenantDataSourceProvider.DEFAULT_TENANT_NAME;
     }
-	
+
     @Override
     public boolean validateExistingCurrentSessions() {
         return true;
--- Common/kc.web/src/main/java/kc/web/WebApiSecurityConfig.java
+++ Common/kc.web/src/main/java/kc/web/WebApiSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
@@ -39,6 +40,7 @@ public class WebApiSecurityConfig extends ResourceServerConfigurerAdapter {
 	@Autowired
 	private IdSrvAuthorizationCodeTokenResponseClient idTokenResponseClient;
 
+	@Lazy
 	@Autowired
 	private ClientRegistrationRepository clientRegistrationRepository;
 
