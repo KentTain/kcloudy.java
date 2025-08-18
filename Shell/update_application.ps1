2dca70a8e37115cd011be51a51a660170f029411
田长军 <tianchangjun@outlook.com>
2025-08-18 15:03:23 +0800

    统一WebApi的Application.java

--- Shell/update_application.ps1
+++ Shell/update_application.ps1
@@ -1,33 +1,31 @@
 # PowerShell script to standardize Application.java files across Web projects
 $baseDir = "d:\Project\kcloudy\java\WebApi"
 $template = @'
-package kc.web;
+package kc.webapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import kc.database.repository.TreeNodeRepositoryFactoryBean;
-import kc.web.util.SpringContextUtil;
 
 @SpringBootApplication
 @ServletComponentScan
-@EntityScan({ "kc.framework.base", "kc.model.MODULE" })
-@ComponentScan({ "kc.mapping.*", "kc.service.*", "kc.service.webapiservice", "kc.web.*", "kc.web" })
-@EnableJpaRepositories(basePackages = { "kc.dataaccess.*", "kc.database" }, repositoryFactoryBeanClass = TreeNodeRepositoryFactoryBean.class)
-public class Application extends kc.web.WebSecurityConfig {
-    public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(Application.class);
-        app.addListeners(new ApplicationPidFileWriter());
-        // 动态注入Bean的工具类的使用：https://www.cnblogs.com/Chary/p/14361830.html
-        ApplicationContext appContext = app.run(args);
-        SpringContextUtil.setApplicationContext(appContext);
-    }
+@EntityScan({ "kc.framework.base", "kc.model" })
+@ComponentScan({ "kc.mapping.*", "kc.service.*", "kc.service.webapiservice", "kc.web.*", "kc.web.multitenancy",
+		"kc.webapi.*" })
+@EnableJpaRepositories(basePackages = { "kc.dataaccess.*",
+		"kc.database" }, repositoryFactoryBeanClass = TreeNodeRepositoryFactoryBean.class)
+public class Application extends kc.web.WebApiSecurityConfig {
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(Application.class);
+		app.addListeners(new ApplicationPidFileWriter());
+		app.run(args);
+	}
 }
 '@
 
@@ -45,7 +43,7 @@ $moduleModelMap = @{
 
 # Process each module
 foreach ($module in $moduleModelMap.Keys) {
-    $appFile = Join-Path $baseDir "kc.webapi.$module\src\main\java\kc\web\Application.java"
+    $appFile = Join-Path $baseDir "kc.webapi.$module\src\main\java\kc\webapi\Application.java"
     $modelPackage = $moduleModelMap[$module]
     
     # Create the content with the correct model package
